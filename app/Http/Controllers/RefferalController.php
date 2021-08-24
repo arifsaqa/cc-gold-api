e1ce68c8feedbc0cfb077d2dc1dcd5f567f4a739
Haidar Rais <abdillah.fams@gmail.com>
2021-08-25 04:44:48 +0700

    referrals

--- app/Http/Controllers/RefferalController.php
+++ app/Http/Controllers/RefferalController.php
@@ -48,12 +48,12 @@ class RefferalController extends Controller
     public function store(Request $request)
     {
         $refferal = Refferal::where('refferal', '=', $request->refferal)->first();
-        $userList = $refferal->userList;
-        if ($userList != null) {
-            $userList = json_decode($refferal->userList);
-            array_push($userList, $request->userId);
-        }
-        $refferal->update(['userList' => $userList]);
+        // if ($userList != null) {
+        //     $userList = json_decode($refferal->userList);
+        //     array_push($userList, $request->userId);
+        //     $refferal->update(['userList' => $userList]);
+        // }
+        $refferal->userList = "1";
         $refferal->save();
 
         return response()->json([
