b33514fa3117cbf325b7234b4e02db6d212a9511
Haidar Rais <abdillah.fams@gmail.com>
2021-08-25 05:16:43 +0700

    numbers

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -213,7 +213,7 @@ class AuthController extends Controller
     }
     public function getAllNumbers()
     {
-        $numbers = User::pluck('phone')->all();
+        $numbers = User::pluck('phone')->where('phone', '!=', null)->get();
         return response()->json([
             'status' => 0,
             'numbers' => $numbers
