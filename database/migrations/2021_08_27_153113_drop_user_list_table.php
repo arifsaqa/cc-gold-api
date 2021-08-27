1ec2102e9c6b92b2c40f03d108abbbbcee8733fc
Haidar Rais <abdillah.fams@gmail.com>
2021-08-27 22:32:34 +0700

    home

---
+++ database/migrations/2021_08_27_153113_drop_user_list_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DropUserListTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('promos', function (Blueprint $table) {
+            $table->dropColumn('user_list');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
