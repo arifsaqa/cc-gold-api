90796bfafedd178f5885fdb6b307d198254e88e5
Haidar Rais <abdillah.fams@gmail.com>
2021-08-28 00:19:04 +0700

    path

--- app/Http/Controllers/PaymentMethodController.php
+++ app/Http/Controllers/PaymentMethodController.php
@@ -35,7 +35,7 @@ class PaymentMethodController extends Controller
         $request->image->move(public_path('payments'), $imageName);
         PaymentMethod::create([
             'name' => $request->get('name'),
-            'logo' =>  'public/payment/'.$imageName,
+            'logo' =>  asset('public/payments/'.$imageName),
         ]);
 
         return redirect()->back()->with('success', 'Upload Metode Pembayaran berhasil');
