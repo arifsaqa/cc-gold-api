7ee6b7023640b178977007d7b7150c5c775f90fb
Haidar Rais <abdillah.fams@gmail.com>
2021-08-28 00:23:15 +0700

    path

--- app/Http/Controllers/PromoController.php
+++ app/Http/Controllers/PromoController.php
@@ -50,7 +50,7 @@ class PromoController extends Controller
             'title' => $request->get('title'),
             'description' => $request->get('description'),
             'discount' => $request->get('discount'),
-            'image' =>  'images/promo/'.$imageName,
+            'image' =>  asset('images/promo/'.$imageName),
         ]);
 
         return redirect()->back()->with('success', 'Upload Promo berhasil');
