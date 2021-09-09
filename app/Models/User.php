d128b21841fe48bb71d853eb3c8fcadc3cd584e0
Haidar Rais <abdillah.fams@gmail.com>
2021-09-09 17:05:54 +0700

    pagination

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,8 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
+    public function transaction()
+    {
+        $this->hasMany(Transaction::class);
+    }
 }
