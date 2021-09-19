59f1af0fed73f611a712ddeacf8b57e0e5f31bf6
Haidar Rais <abdillah.fams@gmail.com>
2021-09-19 23:53:32 +0700

    buy

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -7,65 +7,80 @@
             <h1>Dashboard</h1>
         </div>
         <div class="row">
-            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-            <div class="card card-statistic-1">
-                <div class="card-icon bg-primary">
-                <i class="far fa-user"></i>
-                </div>
-                <div class="card-wrap">
-                <div class="card-header">
-                    <h4>Harga Beli</h4>
-                </div>
-                <div class="card-body">
-                    {{$buy_latest->price ?? 0}}
-                </div>
-                </div>
-            </div>
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-            <div class="card card-statistic-1">
-                <div class="card-icon bg-danger">
-                <i class="far fa-newspaper"></i>
-                </div>
-                <div class="card-wrap">
-                <div class="card-header">
-                    <h4>Harga Jual</h4>
-                </div>
-                <div class="card-body">
-                    {{$sell_latest->price ?? 0}}
-                </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-12">
+                <div class="card card-statistic-1">
+                    <div class="card-icon bg-primary">
+                    <i class="far fa-user"></i>
+                    </div>
+                    <div class="card-wrap">
+                    <div class="card-header">
+                        <h4>Harga Beli</h4>
+                    </div>
+                    <div class="card-body">
+                        {{$buy_latest->price ?? 0}}
+                    </div>
+                    </div>
                 </div>
             </div>
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-            <div class="card card-statistic-1">
-                <div class="card-icon bg-warning">
-                <i class="far fa-file"></i>
-                </div>
-                <div class="card-wrap">
-                <div class="card-header">
-                    <h4>Transaksi Berhasil</h4>
-                </div>
-                <div class="card-body">
-                    {{count($transaction_completed)}}
-                </div>
-                </div>
-            </div>
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-            <div class="card card-statistic-1">
-                <div class="card-icon bg-success">
-                <i class="fas fa-circle"></i>
-                </div>
-                <div class="card-wrap">
-                <div class="card-header">
-                    <h4>Transaksi Gagal</h4>
-                </div>
-                <div class="card-body">
-                    {{count($transaction_failed)}}
-                </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-12">
+                <div class="card card-statistic-1">
+                    <div class="card-icon bg-danger">
+                    <i class="far fa-newspaper"></i>
+                    </div>
+                    <div class="card-wrap">
+                    <div class="card-header">
+                        <h4>Harga Jual</h4>
+                    </div>
+                    <div class="card-body">
+                        {{$sell_latest->price ?? 0}}
+                    </div>
+                    </div>
                 </div>
             </div>
+            <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                <div class="card card-statistic-1">
+                    <div class="card-icon bg-warning">
+                    <i class="far fa-file"></i>
+                    </div>
+                    <div class="card-wrap">
+                    <div class="card-header">
+                        <h4>Transaksi Berhasil</h4>
+                    </div>
+                    <div class="card-body">
+                        {{count($transaction_completed)}}
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                <div class="card card-statistic-1">
+                    <div class="card-icon bg-success">
+                    <i class="fas fa-circle"></i>
+                    </div>
+                    <div class="card-wrap">
+                    <div class="card-header">
+                        <h4>Transaksi Gagal</h4>
+                    </div>
+                    <div class="card-body">
+                        {{count($transaction_failed)}}
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                <div class="card card-statistic-1">
+                    <div class="card-icon bg-success">
+                    <i class="fas fa-circle"></i>
+                    </div>
+                    <div class="card-wrap">
+                    <div class="card-header">
+                        <h4>Transaksi Pending</h4>
+                    </div>
+                    <div class="card-body">
+                        {{count($transaction_pending)}}
+                    </div>
+                    </div>
+                </div>
             </div>
         </div>
         </section>
