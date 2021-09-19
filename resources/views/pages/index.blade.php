118b64391feb20b96e6f023a54a292cd444efc26
Haidar Rais <abdillah.fams@gmail.com>
2021-09-20 00:59:24 +0700

    buy

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -55,7 +55,7 @@
             <div class="col-lg-4 col-md-6 col-sm-6 col-12">
                 <div class="card card-statistic-1">
                     <div class="card-icon bg-danger">
-                    <i class="fas fa-circle"></i>
+                        <i class="fas fa-times-circle"></i>
                     </div>
                     <div class="card-wrap">
                     <div class="card-header">
@@ -70,7 +70,7 @@
             <div class="col-lg-4 col-md-6 col-sm-6 col-12">
                 <div class="card card-statistic-1">
                     <div class="card-icon bg-warning">
-                    <i class="fas fa-circle"></i>
+                        <i class="fas fa-exclamation-circle"></i>
                     </div>
                     <div class="card-wrap">
                     <div class="card-header">
