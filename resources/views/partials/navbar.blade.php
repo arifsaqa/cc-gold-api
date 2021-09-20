32e590c213a0d0059156f71e9cb8292862fb2b1e
Haidar Rais <abdillah.fams@gmail.com>
2021-09-20 12:29:05 +0700

    password

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -10,10 +10,13 @@
           <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
             <img alt="image" src="../assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
             <div class="d-sm-none d-lg-inline-block">{{Auth::user()->name}}</div></a>
-              <div class="dropdown-divider"></div>
-              <a href="#" class="dropdown-item has-icon text-danger">
-                <i class="fas fa-sign-out-alt"></i> Logout
-              </a>
+            <div class="dropdown-menu dropdown-menu-right">
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button type="button" class="dropdown-item has-icon text-danger">
+                        <i class="fas fa-sign-out-alt"></i> Logout
+                    </button>
+                </form>
             </div>
           </li>
         </ul>
